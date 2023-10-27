918c3aa6bc7d9fcf080659dc34738ef0d2551062
Igor J <igorjanuszkiewicz997@gmail.com>
2023-10-27 16:13:04 +0200

    minor bisec improvements

--- optBisec.m
+++ optBisec.m
@@ -1,18 +1,22 @@
-function x = bisec(a,b,f)
+function x = optBisec(a,b,f)
 % Autor: Igor Januszkiewicz
 % bisection method
 accuracy = 500*eps;
 maxLoops = 100;
 i = 0;
 x = (a + b)/2;
+fa = f(a);
+fx = f(x);
 while abs(b - a) > 2*accuracy && i < maxLoops
-    if f(a)*f(b) >= 0
+    if fa*fx >= 0
         a = x;
+        fa = fx;
     else 
         b = x;
     end
     x = (a + b)/2;
-    i = i + 1;
+    fx = f(x);
+     i = i + 1;
 end
 
 end % function
--- untitled.m
+++ untitled.m
@@ -1,14 +1,3 @@
-a = 0;
-accuracy = 500*eps();
-b = realmax;
-
-while 1
-    while a + b - accuracy == a + b
-        b = b/2;
-    end
-    a = a + 2*b;
-
-    a = a*2;
-end
-
-a
+clear;
+f = @(x) sin(x*x);
+roots = nlin(f);
